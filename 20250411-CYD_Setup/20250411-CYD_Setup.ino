0d31de50752189f95efcbb24fb86706bd9a16036
ecolibrium-nyc <pg@loisaidalab.org>
2025-05-28 15:47:07 -0400

    3rd Test

---
+++ 20250411-CYD_Setup/20250411-CYD_Setup.ino
@@ -0,0 +1,27 @@
+// #include "ESPAsyncWebServer.h"
+// #include <SensirionI2CSen5x.h>
+// #include <SensirionI2CScd4x.h> 
+// Downloading TFT_eSPI@2.5.43
+// Downloading XPT2046_Touchscreen@1.4.0
+// esp32:esp32-arduino-libs@idf-release_v5.4-2f7dcd86-v1
+// Downloading WiFiManager@2.0.17
+
+// Board: ESP32-2432S028R (Board, Keep Scrolling)
+
+// Next Steps: 
+// - Update the 'Indoor_Webserver.ino' to use Sen66x Sensor instead of Sen4/Sen5
+
+// Ecolibrium Repo: https://github.com/ecolibrium-nyc/Ecolibrium2023-Sensors/blob/main/Indoor%20Module/Full%20Code/Indoor_Webserver.ino
+// Sen66x Repo: https://github.com/Sensirion/arduino-i2c-sen66
+// ESP32 (CYD) Repo: https://github.com/witnessmenow/ESP32-Cheap-Yellow-Display
+// ESP 32 Repo: https://github.com/witnessmenow/ESP32-Cheap-Yellow-Display
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
