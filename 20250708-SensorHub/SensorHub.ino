87a362fb01339463b268139be39f1a1eff4a6047
Shhmames <101753462+Shhmames@users.noreply.github.com>
2025-07-08 17:55:06 -0400

    Initial testing with NTP and ESP-NOW with Sensor Hub CYD

---
+++ 20250708-SensorHub/SensorHub.ino
@@ -0,0 +1,145 @@
+#include <WiFi.h>
+#include "time.h"
+#include <esp_now.h>
+
+
+// MAC: 5c:01:3b:4f:8c:80//
+
+uint8_t broadcastAddress1[] = {0x5c, 0x01, 0x3b, 0x4f, 0x8c, 0x80};
+
+const char* ssid     = "Loisaida";
+const char* password = "VivaLoisaida";
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = -18000;
+const int   daylightOffset_sec = 3600;
+
+// Station Mode Credentials
+const char* sta_ssid = "Loisaida";
+const char* sta_password = "VivaLoisaida";
+
+// AP Mode Credentials
+const char* ap_ssid = "SensorHub_AP";
+const char* ap_password = "SensorHub_AP123";
+
+typedef struct test_struct {
+  int x;
+  int y;
+} test_struct;
+
+test_struct test;
+
+esp_now_peer_info_t peerInfo;
+
+void setup(){
+
+  Serial.begin(115200);
+
+  // Dual Mode Setup 
+  WiFi.mode(WIFI_AP_STA);  // Enable both AP and Station modes
+  WiFi.softAP(ap_ssid, ap_password);  // Start the access point
+  WiFi.begin(sta_ssid, sta_password); // Connect to existing network
+
+
+  // Connect to Wi-Fi
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  
+  // Init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
+
+  //disconnect WiFi as it's no longer needed
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_OFF);
+
+// For ESPnow
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+  
+  esp_now_register_send_cb(OnDataSent);
+   
+  // register peer
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+  // register first peer  
+  memcpy(peerInfo.peer_addr, broadcastAddress1, 6);
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+}
+
+void loop(){
+  
+  test.x = random(0,20);
+  test.y = random(0,20);
+ 
+  esp_err_t result = esp_now_send(0, (uint8_t *) &test, sizeof(test_struct));
+   
+  if (result == ESP_OK) {
+    Serial.println("Sent with success");
+  }
+  else {
+    Serial.println("Error sending the data");
+  }
+  delay(2000);
+  printLocalTime();
+}
+
+void printLocalTime(){
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  Serial.print("Day of week: ");
+  Serial.println(&timeinfo, "%A");
+  Serial.print("Month: ");
+  Serial.println(&timeinfo, "%B");
+  Serial.print("Day of Month: ");
+  Serial.println(&timeinfo, "%d");
+  Serial.print("Year: ");
+  Serial.println(&timeinfo, "%Y");
+  Serial.print("Hour: ");
+  Serial.println(&timeinfo, "%H");
+  Serial.print("Hour (12 hour format): ");
+  Serial.println(&timeinfo, "%I");
+  Serial.print("Minute: ");
+  Serial.println(&timeinfo, "%M");
+  Serial.print("Second: ");
+  Serial.println(&timeinfo, "%S");
+
+  Serial.println("Time variables");
+  char timeHour[3];
+  strftime(timeHour,3, "%H", &timeinfo);
+  Serial.println(timeHour);
+  char timeWeekDay[10];
+  strftime(timeWeekDay,10, "%A", &timeinfo);
+  Serial.println(timeWeekDay);
+  Serial.println();
+}
+
+
+// For ESPnow
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  char macStr[18];
+  Serial.print("Packet to: ");
+  // Copies the sender mac address to a string
+  snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+           mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+  Serial.print(macStr);
+  Serial.print(" send status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
+}
