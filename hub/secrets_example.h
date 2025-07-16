b769cb906860f28e3cedf2b8f9359e5a2c714f56
Gaudi <asirwatham.gautaman@gmail.com>
2025-07-16 16:20:30 -0400

    adding sender

---
+++ hub/secrets_example.h
@@ -0,0 +1,7 @@
+#pragma once
+
+const char* sta_ssid     = "Your_STA_SSID";
+const char* sta_password = "Your_STA_Password";
+
+const char* ap_ssid      = "Your_AP_SSID";
+const char* ap_password  = "Your_AP_Password";
