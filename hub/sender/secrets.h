8bf42f74db4b51c7168e109f6c1e191b0566674b
Gaudi <asirwatham.gautaman@gmail.com>
2025-07-21 12:06:27 -0400

    without secrets

--- hub/sender/secrets.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-const char* sta_ssid     = "Loisaida";
-const char* sta_password = "VivaLoisaida";
-
-const char* ap_ssid      = "SensorHub_AP";
-const char* ap_password  = "SensorHub_AP123";
