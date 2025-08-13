94858062ed98dd132126e3b7abd51b32665aae04
Gaudi <asirwatham.gautaman@gmail.com>
2025-08-13 14:16:54 -0400

    I had trouble pushing my changes so they are in a new branch

--- Bluetti_ESP32/Bluetti_ESP32.ino
+++ Bluetti_ESP32/Bluetti_ESP32.ino
@@ -16,24 +16,17 @@
 #include "ui.h"
 #include "screens.h"
 
+// --- (All your declarations remain the same) ---
+
 unsigned long lastTime1 = 0;
 unsigned long timerDelay1 = 3000;
+
+// ... (All functions down to setup() remain exactly the same) ...
 
 void updateArc(uint8_t battery_level) {
     if (battery_level > 100) battery_level = 100;
     lv_arc_set_value(objects.battery_arc, battery_level);
 }
-
-// A library for interfacing with the touch screen
-//
-// Can be installed from the library manager (Search for "XPT2046")
-//https://github.com/PaulStoffregen/XPT2046_Touchscreen
-// ----------------------------
-// Touch Screen pins
-// ----------------------------
-
-// The CYD touch uses some non default
-// SPI pins
 
 #define XPT2046_IRQ 36
 #define XPT2046_MOSI 32
@@ -43,12 +36,8 @@ void updateArc(uint8_t battery_level) {
 SPIClass touchscreenSpi = SPIClass(VSPI);
 XPT2046_Touchscreen touchscreen(XPT2046_CS, XPT2046_IRQ);
 uint16_t touchScreenMinimumX = 200, touchScreenMaximumX = 3700, touchScreenMinimumY = 240, touchScreenMaximumY = 3800;
-
-/*Set to your screen resolution*/
 #define TFT_HOR_RES 320
 #define TFT_VER_RES 240
-
-/*LVGL draw into this buffer, 1/10 screen size usually works well. The size is in bytes*/
 #define DRAW_BUF_SIZE (TFT_HOR_RES * TFT_VER_RES / 10 * (LV_COLOR_DEPTH / 8))
 
 #if LV_USE_LOG != 0
@@ -59,80 +48,51 @@ void my_print(lv_log_level_t level, const char *buf) {
 }
 #endif
 
-/* LVGL calls it when a rendered image needs to copied to the display*/
 void my_disp_flush(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map) {
-  /*Call it to tell LVGL you are ready*/
   lv_disp_flush_ready(disp);
 }
-
-/*Read the touchpad*/
 void my_touchpad_read(lv_indev_t *indev, lv_indev_data_t *data) {
   if (touchscreen.touched()) {
     TS_Point p = touchscreen.getPoint();
-    //Some very basic auto calibration so it doesn't go out of range
     if (p.x < touchScreenMinimumX) touchScreenMinimumX = p.x;
     if (p.x > touchScreenMaximumX) touchScreenMaximumX = p.x;
     if (p.y < touchScreenMinimumY) touchScreenMinimumY = p.y;
     if (p.y > touchScreenMaximumY) touchScreenMaximumY = p.y;
-    //Map this to the pixel position
-    data->point.x = map(p.x, touchScreenMinimumX, touchScreenMaximumX, 1, TFT_HOR_RES); /* Touchscreen X calibration */
-    data->point.y = map(p.y, touchScreenMinimumY, touchScreenMaximumY, 1, TFT_VER_RES); /* Touchscreen Y calibration */
+    data->point.x = map(p.x, touchScreenMinimumX, touchScreenMaximumX, 1, TFT_HOR_RES);
+    data->point.y = map(p.y, touchScreenMinimumY, touchScreenMaximumY, 1, TFT_VER_RES);
     data->state = LV_INDEV_STATE_PRESSED;
   } else {
     data->state = LV_INDEV_STATE_RELEASED;
   }
 }
 
-lv_indev_t *indev;      //Touchscreen input device
-uint8_t *draw_buf;      //draw_buf is allocated on heap otherwise the static area is too big on ESP32 at compile
-uint32_t lastTick = 0;  //Used to track the tick timer
-
-//CN1 Extended IO PIN 3.3v(wire red) -> 27(wire yellow) -> 22(wire blue) -> GND(black)
-//P3 Extended IO PIN 21(red - Can't use as it's the backlight) -> 22(wire yellow) -> 35(wire blue) -> GND(black)
-//https://randomnerdtutorials.com/esp32-cyd-lvgl-temperature-ds18b20/
-//https://esp32io.com/tutorials/esp32-rgb-led
-//pins and setups
-const int ledPin22 = 22;  //
+lv_indev_t *indev;
+uint8_t *draw_buf;
+uint32_t lastTick = 0;
+const int ledPin22 = 22;
 
 void setup() {
 
-  //Some basic info on the Serial console
   String LVGL_Arduino = "LVGL demo ";
   LVGL_Arduino += String('V') + lv_version_major() + "." + lv_version_minor() + "." + lv_version_patch();
   Serial.begin(115200);
+  delay(2000); // Add a small delay to ensure the serial monitor can connect
   Serial.println(LVGL_Arduino);
 
-  //Initialise the touchscreen
-  touchscreenSpi.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS); /* Start second SPI bus for touchscreen */
-  touchscreen.begin(touchscreenSpi);                                         /* Touchscreen init */
-  touchscreen.setRotation(3);                                                /* Inverted landscape orientation to match screen */
+  touchscreenSpi.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
+  touchscreen.begin(touchscreenSpi);
+  touchscreen.setRotation(3);
 
-  //Initialise LVGL GUI
   lv_init();
-
   draw_buf = new uint8_t[DRAW_BUF_SIZE];
   lv_display_t *disp;
   disp = lv_tft_espi_create(TFT_HOR_RES, TFT_VER_RES, draw_buf, DRAW_BUF_SIZE);
-
-  //Initialize the XPT2046 input device driver
   indev = lv_indev_create();
   lv_indev_set_type(indev, LV_INDEV_TYPE_POINTER);
   lv_indev_set_read_cb(indev, my_touchpad_read);
-
-  //Done
   Serial.println("LVGL Setup done");
-
-  //Integrate EEZ Studio GUI
   ui_init();
-
-  //Set pin modes
-  //CN1 Extended IO PIN 3.3v(red) -> 27(yellow) -> 22(blue) -> GND(black)
-  //P3 Extended IO PIN 21(red - also dims backlight) -> 22(wire yellow) -> 35(wire blue) -> GND(black)
-  pinMode(ledPin22, OUTPUT);  // Sets it to OUTPUT
-
-  //Register event handler
-  //
-  //lv_obj_add_event_cb(objects.arc_dimmer0to100, arc_dimmer_event_handler, LV_EVENT_VALUE_CHANGED, objects.label_dim_value);
+  pinMode(ledPin22, OUTPUT);
 
   #ifdef RELAISMODE
     pinMode(RELAIS_PIN, OUTPUT);
@@ -147,31 +107,56 @@ void setup() {
   #ifdef DISPLAYSSD1306
     initDisplay();
   #endif
+
+  // --- MINIMAL CHANGE SECTION ---
+  // The order of these next three calls is CRITICAL for TLS to work.
+
+  // 1. Connect to WiFi first.
   initBWifi(false);
+
+  // 2. Sync time now that we have internet.
+  syncNtpTime();
+  
+  // 3. Now initialize MQTT.
+  initMQTT();
+
+  // 4. Initialize Bluetooth last.
+  // (It's a blocking call and should not prevent network setup)
   initBluetooth();
-  // initMQTT();
+
+  // --- END OF CHANGE SECTION ---
+
   #ifdef DISPLAYSSD1306
     wrDisp_Status("Running!");
   #endif
-  
 }
 
 static void arc_dimmer_event_handler(lv_event_t *e) {
-  lv_event_code_t code = lv_event_get_code(e);              //Get the event code
-  lv_obj_t *arc = (lv_obj_t *)lv_event_get_target(e);       //Switch that generated the event
-  lv_obj_t *label = (lv_obj_t *)lv_event_get_user_data(e);  //Label or other UI elements we want to update (Optional)
+  lv_event_code_t code = lv_event_get_code(e);
+  lv_obj_t *arc = (lv_obj_t *)lv_event_get_target(e);
+  lv_obj_t *label = (lv_obj_t *)lv_event_get_user_data(e);
 
   if (code == LV_EVENT_VALUE_CHANGED) {
-    int brightness = lv_arc_get_value(arc);  // 0 to 100 so we need to map it 0 to 255
+    int brightness = lv_arc_get_value(arc);
     brightness = map(brightness, 0, 100, 0, 255);
     analogWrite(ledPin22, brightness);
     lv_label_set_text_fmt(label, "%d", brightness);
-    //Set the LED UI element to be more visible
     lv_obj_set_style_bg_opa(objects.led1, brightness, LV_PART_MAIN | LV_STATE_DEFAULT);
   }
 }
 
-//Requires LVGL 9.0+
+void syncNtpTime() {
+    configTime(0, 0, "pool.ntp.org");
+    Serial.print("Waiting for NTP time sync... ");
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo, 15000)) { // 15 second timeout
+        Serial.println(" FAILED. TLS connections may fail.");
+        return;
+    }
+    Serial.println(" SUCCESS.");
+    Serial.println(&timeinfo, "Current time: %A, %B %d %Y %H:%M:%S");
+}
+
 void loop() {
   const char *batPercentage = getBatteryPercent();
   const char *AC_InputPower = getAC_Input();
@@ -188,35 +173,30 @@ void loop() {
   }
 
   if (AC_InputPower != NULL && isdigit(*AC_InputPower)) {
-    char result[100];   // array to hold the result.
-    strcpy(result,AC_InputPower); // copy string one into the result.
-    strcat(result," Watts"); // append string two to the result.
-
+    char result[100];
+    strcpy(result,AC_InputPower);
+    strcat(result," Watts");
     lv_label_set_text(objects.ac_input_num, result);
   }
 
   if (AC_OutputPower != NULL && isdigit(*AC_OutputPower)) {
-    char result[100];   // array to hold the result.
-    strcpy(result,AC_OutputPower); // copy string one into the result.
-    strcat(result," Watts"); // append string two to the result.
-    
+    char result[100];
+    strcpy(result,AC_OutputPower);
+    strcat(result," Watts");
     lv_label_set_text(objects.ac_output_num, result);
   }
 
   if (DC_InputPower != NULL && isdigit(*DC_InputPower)) {
-    char result[100];   // array to hold the result.
-    strcpy(result,DC_InputPower); // copy string one into the result.
-    strcat(result," Watts"); // append string two to the result.
-
+    char result[100];
+    strcpy(result,DC_InputPower);
+    strcat(result," Watts");
     lv_label_set_text(objects.dc_input_num, result);
   }
 
   if (DC_OutputPower != NULL && isdigit(*DC_OutputPower)) {
-    char result[100];   // array to hold the result.
-    strcpy(result,DC_OutputPower); // copy string one into the result.
-    strcat(result," Watts"); // append string two to the result.
-
-    // lv_label_set_text(objects.dc_output_num, DC_OutputPower);
+    char result[100];
+    strcpy(result,DC_OutputPower);
+    strcat(result," Watts");
     lv_label_set_text(objects.dc_output_num, result);
   }
 
@@ -224,8 +204,7 @@ void loop() {
     int offChecker = strchr(AC_Stats, '0') != NULL;
     if (offChecker == 0) {
       lv_obj_set_style_text_color(objects.ac_status, lv_color_hex(0xff00ff26), LV_PART_MAIN | LV_STATE_DEFAULT);
-    }
-    else {
+    } else {
       lv_obj_set_style_text_color(objects.ac_status, lv_color_hex(0xffc00000), LV_PART_MAIN | LV_STATE_DEFAULT);
     }
   }
@@ -234,24 +213,24 @@ void loop() {
     int offChecker = strchr(DC_Stats, '0') != NULL;
     if (offChecker == 0) {
       lv_obj_set_style_text_color(objects.dc_status, lv_color_hex(0xff00ff26), LV_PART_MAIN | LV_STATE_DEFAULT);
-    }
-    else {
+    } else {
       lv_obj_set_style_text_color(objects.dc_status, lv_color_hex(0xffc00000), LV_PART_MAIN | LV_STATE_DEFAULT);
     }
   }
 
-  //0xff00ff26 green
-  //0xffc00000 red
-
-  lv_tick_inc(millis() - lastTick);  //Update the tick timer. Tick is new for LVGL 9
+  lv_tick_inc(millis() - lastTick);
   lastTick = millis();
-  lv_timer_handler();  //Update the UI
+  lv_timer_handler();
 
   #ifdef DISPLAYSSD1306
     //handleDisplay();
   #endif
+  
+  // --- MINIMAL CHANGE ---
+  // Add this call to the loop. It handles reconnects and publishing.
+  handleMQTT(); 
+
   handleBluetooth();
-  //handleMQTT(); 
   handleWebserver();
   delay(5);
 }
