400382e1f895fdedf375313d2e51414d12f45c1e
Jamcre <jmcrespo123@gmail.com>
2025-08-14 10:24:47 -0400

    Directory clean up

--- changed configs/pio contents/build/esp32dev/FrameworkArduino/USBMSC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.o: \
- /Users/oscar/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
- /Users/oscar/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- /Users/oscar/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- changed configs/pio contents/build/esp32dev/lib3cd/lvgl/draw/nxp/vglite/lv_draw_vglite_img.c.d
+++
@@ -1,8 +0,0 @@
-.pio/build/esp32dev/lib3cd/lvgl/draw/nxp/vglite/lv_draw_vglite_img.c.o: \
- .pio/libdeps/esp32dev/lvgl/src/draw/nxp/vglite/lv_draw_vglite_img.c \
- .pio/libdeps/esp32dev/lvgl/src/draw/nxp/vglite/lv_draw_vglite.h \
- .pio/libdeps/esp32dev/lvgl/src/draw/nxp/vglite/../../../lv_conf_internal.h \
- .pio/libdeps/esp32dev/lvgl/src/draw/nxp/vglite/../../../lv_conf_kconfig.h \
- /Users/oscar/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h \
- /Users/oscar/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/include/esp_common/include/esp_attr.h \
- .pio/libdeps/esp32dev/lvgl/src/draw/nxp/vglite/../../../../../lv_conf.h
